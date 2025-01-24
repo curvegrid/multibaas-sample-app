219ee4bae38bf3315c822d0ed76280292754fc79
Daniel Briskin <daniel.briskin@gmail.com>
2025-01-24 10:33:38 +0900

    comment clarity

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -7,8 +7,8 @@ let deployerPrivateKey = '';
 let deploymentEndpoint = '';
 let ethChainID = 1337;
 let adminApiKey = '';
-let web3Key = ''; // optional
-let rpcUrl = ''; // optional
+let web3Key = '';
+let rpcUrl = ''; // must be defined in the deployment config if the web3Key is not defined
 
 if (process.env['HARDHAT_NETWORK']) {
   const CONFIG_FILE = path.join(__dirname, `./deployment-config.${process.env['HARDHAT_NETWORK']}`);
@@ -19,7 +19,7 @@ if (process.env['HARDHAT_NETWORK']) {
   } = require(CONFIG_FILE));
 }
 
-const web3Url = web3Key ? `${deploymentEndpoint}/web3/${web3Key}` : rpcUrl;
+const web3Url = web3Key && web3Key !== '' ? `${deploymentEndpoint}/web3/${web3Key}` : rpcUrl;
 
 const config: HardhatUserConfig = {
   networks: {
